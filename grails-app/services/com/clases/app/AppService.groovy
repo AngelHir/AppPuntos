65887f040699c6c831797840aae41b86f0c76cd9
Angel Hir <angeltorres.nts13@gmail.com>
2023-08-04 13:54:08 -0600

    Push inicial

--- grails-app/services/com/clases/app/AppService.groovy
+++ grails-app/services/com/clases/app/AppService.groovy
@@ -6,6 +6,7 @@ import grails.gorm.transactions.Transactional
 class AppService {
 
     def crearProductos() {
-        new Producto(nombre: 'Galletas Oreo 60 grms', descripcion: 'unidad de galleta oreo marca Nacional de 60 grms', codigoBarra:'00001x45', stock: 12, precio: 18.50 )
+        new Producto(nombre: 'Galletas Oreo 60 grms', descripcion: 'unidad de galleta oreo marca Nacional de 60 grms', codigoBarra:'00001x45',
+                stock: 12, precio: 18.50 ).save(flush: true)
     }
 }
