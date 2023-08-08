1a1857d066bca607edb02ed8b3765fa2d6bdef56
Angel Hir <angeltorres.nts13@gmail.com>
2023-08-08 17:58:22 -0600

    Push

--- grails-app/domain/com/clases/app/CatalogoGeneral.groovy
+++ grails-app/domain/com/clases/app/CatalogoGeneral.groovy
@@ -2,6 +2,27 @@ package com.clases.app
 
 class CatalogoGeneral {
 
+    Boolean activo=true
+    String nombre
+    String descripcion
+    String codigo
+    String clasificacion
+    BigDecimal precio
+    int disponibilidad
+    String proveedor
+
+
     static constraints = {
+        nombre nullable: false, blank: false
+        descripcion nullable: false, blank: false
+        codigo length:8, nullable: false, blank: false, unique: true
+        clasificacion nullable: false, blank: false
+        precio nullable: false, blank: false
+        disponibilidad nullable: false, blank: false
+        proveedor nullable: false, blank: false
+    }
+
+    static mapping = {
+        id generator: 'sequence', params: [sequence_name: 'catalogo_general_seq']
     }
 }
