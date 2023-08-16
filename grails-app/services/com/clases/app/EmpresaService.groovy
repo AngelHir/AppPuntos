9454e6ad08d2bdb96fe4f9d858c934bfbd09a235
Angel <angeltorres.nts13@gmail.com>
2023-08-16 16:36:45 -0600

    Modificaciones 16/08/23

---
+++ grails-app/services/com/clases/app/EmpresaService.groovy
@@ -0,0 +1,11 @@
+package com.clases.app
+
+import com.software.componente.app.ObjectException
+import com.software.componente.app.Message
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class EmpresaService {
+
+
+}
